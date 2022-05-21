27ad25fe6c714a163589e7d4c33db4bceef25283
Valensceﮩﮩ٨ﮩﮩ٨ﮩ <pradolifu@gmail.com>
2022-05-21 20:00:51 +1000

    Update

--- app/scripts/queueList.py
+++ app/scripts/queueList.py
@@ -1,32 +1,69 @@
 def queueList():
+
     totalOrder = [
-        {"type":"dessert","name":"cake","price":int(7)},
-        {"type":"burger","name":"beef","price":int(15)},
-        {"type":"drink","name":"lemonade","price":int(5)}
+        {
+            "type": "dessert",
+            "name": "cake",
+            "price": 7
+        },
+        {
+            "type": "burger",
+            "name": "beef",
+            "price": 15
+        },
+        {
+            "type": "drink",
+            "name": "lemonade",
+            "price": 5
+        }
     ]
+
     queueList = [
-        {"primaryOrder"},
-        {"secondaryOrder"},
-        {"tertiaryOrder"}
+        [
+            {
+                "type": "dessert",
+                "name": "cake",
+                "price": 7
+            },
+            {   "type": "burger",
+                "name": "beef",
+                "price": 15
+            },
+            {
+                "type": "drink",
+                "name": "lemonade",
+                "price": 5
+            }
+        ],
+        [
+            {
+                "type": "salad",
+                "name": "caeser",
+                "price": 12
+            },
+            {
+                "type": "drink",
+                "name": "lemonade",
+                "price": 5
+            },
+            {
+                "type": "burger",
+                "name": "beef",
+                "price": 15
+            }
+        ],
+        [
+            {
+                "type": "dessert",
+                "name": "fudge",
+                "price": 5
+            },
+            {
+                "type": "drink",
+                "name": "lemonade",
+                "price": 5
+            }
+        ]
     ]
-    queueList.extend(totalOrder)
-    print(queueList)
-    selection1 = input("Would you like to remove an order? y/n")
-    if selection1 == "n":
-        queueList()
-    if selection1 == "y":
-        print("Alrightie")
-    selection2 = input("Select order to remove")
-    if selection2 == "1":
-        totalOrder.pop(1)
-        print(queueList)
-    elif selection2 == "2":
-        totalOrder.pop(2)
-        print(queueList)
-    elif selection2 == "3":
-        totalOrder.pop(3)
-        print(queueList)
-    elif selection2 =="Latest":
-        totalOrder.pop(-1)
-        print(queueList)
+    
 queueList()
