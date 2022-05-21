c03fa0eb71c7648f08d717529d69ccc500d7d628
Valensceﮩﮩ٨ﮩﮩ٨ﮩ <pradolifu@gmail.com>
2022-05-21 20:00:30 +1000

    Update

--- app/scripts/calcBill.py
+++ app/scripts/calcBill.py
@@ -1,18 +1,34 @@
 # FOR item IN orderlist
-
 #      SET price = orderItem.price
-
 #      SET total = total + price
 
 def calcBill():
+
     totalOrder = [
-        {"type":"dessert","name":"cake","price":int(7)},
-        {"type":"burger","name":"beef","price":int(15)},
-        {"type":"drink","name":"lemonade","price":int(5)}
+        {
+            "type": "dessert",
+            "name": "cake",
+            "price": 7
+        },
+        {
+            "type": "burger",
+            "name": "beef",
+            "price": 15
+        },
+        {
+            "type": "drink",
+            "name": "lemonade",
+            "price": 5
+        }
     ]
+
     total = 0
+
     for row in totalOrder:
+
         price = row["price"]
         total = total + price
+
     print(total)
+    
 calcBill()
